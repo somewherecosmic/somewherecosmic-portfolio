87856c68c352ea5742bc1166edc230f988795481
Ethan Hall <ethanhall7742@gmail.com>
2023-08-30 14:42:02 +0100

    Styling Header component, changes to Hero component

--- somewherecosmic-portfolio/src/components/header/Header.tsx
+++ somewherecosmic-portfolio/src/components/header/Header.tsx
@@ -1,5 +1,4 @@
 import "./Header.css"
-import shigeo from "../../assets/shigeo.jpeg";
 
 import { Link, Outlet } from "react-router-dom";
 
@@ -7,18 +6,15 @@ import { Link, Outlet } from "react-router-dom";
 function Header() {
     return(
         <>
-        <div className="logo">
-            <img src={shigeo} alt=""/>
-        </div>
-        <button className="nav-toggle" aria-label="toggle navigation">
-            <span className="stack"></span>
-        </button>
+        <div className="flex">
+            <h1 className="title">SomewhereCosmic</h1>
         <nav className="nav">
             <ul className="nav__list">
                 <li className="nav__item"><Link to="/profile" className="nav__link">Profile</Link></li>
                 <li className="nav__item"><Link to="/projects" className="nav__link">Projects</Link></li>
             </ul>
         </nav>
+        </div>
         <Outlet/>
         </>
     )
--- somewherecosmic-portfolio/src/components/hero/Hero.tsx
+++ somewherecosmic-portfolio/src/components/hero/Hero.tsx
@@ -3,7 +3,7 @@ import "./Hero.css";
 function Hero() {
   return (
     <section className="hero">
-      <h1 className="section__title section__title--hero">placeholder Intro</h1>
+      <h1 className="section__title section__title--hero">Ethan Hall</h1>
       <p className="section__subtitle section__subtitle--hero">
         CS Grad -- FullStack Dev
       </p>
